e76a841ec518d4b72cb021c45d2ae33c17b0a74b
alejandrodiazrive-ship-it <alejandrodiazrive@gmail.com>
2026-02-04 18:47:56 -0600

    refactor: standardize all SQL aliases and comments to English

--- 01_churn_for_type_of_contract.sql
+++ 01_churn_for_type_of_contract.sql
@@ -1,7 +1,7 @@
 SELECT 
     contract, 
-    COUNT(*) AS total_clientes,
-    COUNT(*) FILTER (WHERE Churn = 'Yes') AS desertores,
+    COUNT(*) AS total_clients,
+    COUNT(*) FILTER (WHERE Churn = 'Yes') AS churn_count,
     ROUND(COUNT(*) FILTER (WHERE Churn = 'Yes')::numeric / COUNT(*) * 100, 2) AS churn_rate_pct
 FROM telco_churn
 GROUP BY contract
